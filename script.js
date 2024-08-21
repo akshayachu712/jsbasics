c67a30688cb134e9c2027a4055d42d599c5942e7
akshayachu712 <akshayprasad604@gmail.com>
2024-08-21 21:55:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.write("Hello World");
+document.write("<br>");
+document.write("Hello World");
+console.log("This is a test message")
+console.warn("Warning")
+console.error("error message")
+
+// Single line comment
+/*this
+is a
+multiline comment*/
+
+//let var const 3 types js
+
+let person
+person="anie"
+console.log(person)
+
+var age=19
+console.log(age)
+
+const pi=3.14
+console.log(pi)
+
+//hoisting learn difference btwm let&var interview qn
